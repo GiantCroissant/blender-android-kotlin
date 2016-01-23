d4c023c7ccb267d63f14ee5abe073985290bdd7b
Apprentice <apprentice@giantcroissant.com>
2016-01-23 15:20:11 +0800

    Added initial navigation drawer
    Still trying to get it right by referencing different articles online, thus the resoures files are not unified yet.

--- app/src/main/kotlin/com/giantcroissant/blender/app/MainActivity.kt
+++ app/src/main/kotlin/com/giantcroissant/blender/app/MainActivity.kt
@@ -2,14 +2,28 @@ package com.giantcroissant.blender.app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
 import android.view.Menu
 import android.view.MenuItem
+import android.support.v7.widget.Toolbar
+//import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+//    private var drawer: DrawerLayout = null
+//    private var toolbar: Toolbar = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        //supportActionBar.setHomeAsUpIndicator(R.drawable.ic_menu)
+        supportActionBar.setDisplayHomeAsUpEnabled(true)
+
+        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -30,5 +44,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
     }
 }
